d7f5c2c2c9c7b700d8f280ff2e0183b9b7b06a3f
Maksym Denysenko <mdenyse@softserveinc.com>
2021-11-23 23:32:00 +0200

    Add TaskListAdapter

--- app/src/main/java/com/softserve/todoapp/MainActivity.kt
+++ app/src/main/java/com/softserve/todoapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.softserve.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.softserve.todoapp.adapter.TaskListAdapter
+import com.softserve.todoapp.model.Task
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: TaskListAdapter
+    lateinit var recycler: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recycler = findViewById(R.id.listOfTasks)
+        recycler.layoutManager = LinearLayoutManager(this)
+        adapter = TaskListAdapter(createData())
+        recycler.adapter = adapter
+    }
+
+    fun createData(): List<Task>{
+        return listOf(
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414"),
+            Task("sometext", "todo", "121414")
+        )
     }
 }
---
+++ app/src/main/java/com/softserve/todoapp/model/Task.kt
@@ -0,0 +1,9 @@
+package com.softserve.todoapp.model
+
+data class Task(
+    var title: String,
+    var task: String,
+    var dateCreation: String,
+    var priority: Int = 1
+) {
+}
